99d7589441a273646fdf782f3c224b72ba11e560
Gerald <grenier@ipnl.in2p3.fr>
2016-11-07 18:45:54 +0100

    Add python script for noise estimation

---
+++ python/noise_analysis_script.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+#to be executed in the lib directory where all the needed libraries are installed 
+# usage python <name>.py  list_of_slcio_files
+import grpc
+import sys
+import ROOT
+ROOT.gROOT.Reset()
+
+inputFileNames=grpc.std_string_vec()
+for f in sys.argv[1:]:
+    inputFileNames.push_back(f);
+
+for file in inputFileNames:
+    print file
+
+experience=grpc.GIF_oct2016_ExperimentalSetup()
+numeroBIF=experience.getBIF();
+
+print numeroBIF
+
+BIFtriggerWindow=grpc.int_intervalle(-8,-6)
+NoiseWindowLength=50; #10 microsecond
+
+#start LCIO reader
+lcReader=grpc.LCFactory_getInstance().createLCReader()
+
+#create architecture of listeners
+masterReader=grpc.RawHit_SDHCAL_Data_Reader_From_LCEvent()
+lcReader.registerLCEventListener(masterReader)
+
+noiseReader=grpc.RawHit_SDHCAL_Data_Reader_Noise(experience,NoiseWindowLength)
+noiseReader.setVetoOnBIF()
+noiseReader.setBIFtimeWindow(BIFtriggerWindow)
+masterReader.registerDataListener(noiseReader)
+
+
+hitOccupancy=grpc.RawHit_Occupancy_Listener()
+noiseReader.registerDataListener(hitOccupancy)
+
+
+
+#open file and event loop
+lcReader.open( inputFileNames )
+lcReader.readStream(1000)
+
+#end of event loop
+rootFile=ROOT.TFile("noise.root"  , "RECREATE")
+d=rootFile.mkdir("Noise_10microsec")
+hitOccupancy.saveTo_wrap(ROOT.AsCObject(d),experience)
+
+rootFile.Close()
+
+lcReader.close()
