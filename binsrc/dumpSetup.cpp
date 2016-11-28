b8016cee0e40f2a4d8dfcea69e63f79ca7e70799
Gerald <grenier@ipnl.in2p3.fr>
2016-11-28 14:38:38 +0100

    dump partially DBstate in csv file as well

--- binsrc/dumpSetup.cpp
+++ binsrc/dumpSetup.cpp
@@ -1,7 +1,9 @@
 #include "ILCConfDB.h"
 
 #include <iostream>
+#include <fstream>
 #include <set>
+#include <string>
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +15,8 @@ int main(int argc, char *argv[])
     }
   else
     setupName=argv[1];
+  std::string outfileName=setupName;
+  outfileName+=".csv";
   DBInit::init();
   std::cout << "dumping setup " << setupName << std::endl;
   Setup *mySetup=Setup::getSetup(setupName);
@@ -24,6 +28,10 @@ int main(int argc, char *argv[])
   std::cout << "ASIC conf pointer=" << AsicConf << std::endl;
   std::vector<ConfigObject*>& ASICconfVector=AsicConf->getVector();
   std::cout <<"found " << ASICconfVector.size() << " ASICs in configuration. " << std::endl;
+  std::ofstream outfile(outfileName);
+  outfile << "IS_ASIC;DIF_ID;ASIC_ID;THRESH1;THRESH2;THRESH3;NCHANNELS";
+  for (unsigned int i=0; i<64; ++i) outfile << ";GAIN"<<i;
+  outfile<<std::endl;
   for (std::vector<ConfigObject*>::iterator it=ASICconfVector.begin(); it!=ASICconfVector.end();++it)
     {
       ConfigObject* obj=*it;
@@ -34,8 +42,12 @@ int main(int argc, char *argv[])
 		<< " ASIC_ID=" <<  obj->getInt("HEADER") 
 		<< " THRESHOLDS=" << obj->getInt("B0") << "/" << obj->getInt("B1")<<"/" << obj->getInt("B2") 
 		<< " number of gains=" << gain.size() << " gain set=";
+      outfile<<int(obj->getObjectName()=="ASIC")<<";"<<obj->getInt("DIF_ID")<<";"<<obj->getInt("HEADER")<<";"
+	     <<obj->getInt("B0") << ";" << obj->getInt("B1")<<";" << obj->getInt("B2")<<";"<<gain.size();
       for (std::set<int>::iterator itg=gainset.begin(); itg!=gainset.end(); ++itg) std::cout << *itg << " ";
+      for (std::vector<int>::iterator itgain=gain.begin(); itgain != gain.end(); ++itgain) outfile << ";" << (*itgain);
       std::cout << std::endl;
+      outfile << std::endl;
     }
   delete mySetup;
   return 0;
