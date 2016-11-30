c654805ed6c29b5f737deaa239f38e09b8da9ef0
Gerald <grenier@ipnl.in2p3.fr>
2016-11-30 19:31:16 +0100

    add python script to produce tree for october 2016 GIF results

---
+++ python/produce_run_tree_GIF.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+#results are assumed to be in the directory given as argument to the script 
+#the directory should have the following structure :
+# "argv[1]"/RunNumber/report_RunNumber.txt
+#  argv[1] should only contains subdirectory whose names corresponds to run numbers and contains the expected report file
+
+import sys
+import glob
+import os.path
+import ROOT
+from array import array
+ROOT.gROOT.Reset()
+
+ROOT.gSystem.Load('liblcio')
+ROOT.gSystem.Load('libGRPC_RawHit_Readout_Analyser')
+ROOT.gSystem.Load('libGRPC_RawHit_Readout_Analyser_dict')
+
+directory=sys.argv[1]
+resultsDirectory=glob.glob(directory+'/*')
+
+experience=ROOT.GIF_oct2016_ExperimentalSetup()
+all=ROOT.all_ConfigInfo.instance()
+
+
+runNumBranchObject = array( 'I', [ 0 ] )
+hasConfBranchObject = array( 'B', [0] )
+hasGIFCondBranchObject = array( 'B', [0] )
+Noise = ROOT.RunThresholdCounter('MappedCounters<MappedCounters<SingleCounter> >')() 
+Eff = ROOT.RunThresholdCounter('MappedCounters<MappedCounters<SingleCounter> >')() 
+FakeEff = ROOT.RunThresholdCounter('MappedCounters<MappedCounters<SingleCounter> >')() 
+ConfigInfo = ROOT.Setup_ConfigInfo()
+GIFconditions = ROOT.GIF_Conditions()
+
+f = ROOT.TFile( 'GIF_oct2016.root', 'RECREATE' )
+tree = ROOT.TTree( 'results', 'Run analysis tree' )
+
+tree.Branch( 'runNumber',runNumBranchObject , 'runNumber/i' )
+tree.Branch( 'Noise', Noise, 32000, 0 )
+tree.Branch( 'Efficiency', Eff, 32000, 0 )
+tree.Branch( 'FakeEfficiency', FakeEff, 32000, 0 )
+tree.Branch( 'hasConf', hasConfBranchObject, 'hasConf/b' )
+tree.Branch( 'ConfigInfo', ConfigInfo, 32000, 0 )
+tree.Branch( 'hasGIFCond', hasGIFCondBranchObject, 'hasGIFCond/b' )
+tree.Branch( 'GIFcond', GIFconditions)
+
+for dir in resultsDirectory :
+  runNum=os.path.basename(dir)
+  filename=dir+'/report_'+runNum+'.txt'
+  runNumber=int(runNum)
+  print runNumber
+  runNumBranchObject[0]=runNumber
+  file=ROOT.std.ifstream(filename)
+  Noise.ASCIIread(file)
+  Eff.ASCIIread(file)
+  FakeEff.ASCIIread(file)
+  hasConfBranchObject[0]=ConfigInfo.setToRun(runNumber)
+  hasGIFCondBranchObject[0]=GIFconditions.setToRun(runNumber)
+  tree.Fill()
+
+
+f.Write()
+f.Close()
