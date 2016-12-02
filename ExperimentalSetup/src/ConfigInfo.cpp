28e72850ee160e74618bc854625e995d53102945
Gerald <grenier@ipnl.in2p3.fr>
2016-12-02 15:51:43 +0100

    Fix bug in GIF attenuation computation

--- ExperimentalSetup/src/ConfigInfo.cpp
+++ ExperimentalSetup/src/ConfigInfo.cpp
@@ -65,7 +65,7 @@ float GIF_Conditions::attenuatorFactorApprox(unsigned int att)
   static float plane1app[3]={1,10,100};
   static float plane2app[3]={1,1.5,100};
   static float plane3app[3]={1,2.2,4.6};
-  return plane1app[(att/100)%1000]*plane2app[(att/10)%100]*plane3app[att%10];
+  return plane1app[(att%1000)/100-1]*plane2app[(att%100)/10-1]*plane3app[att%10-1];
 }
 
 float GIF_Conditions::attenuatorFactor(unsigned int att)
@@ -73,7 +73,7 @@ float GIF_Conditions::attenuatorFactor(unsigned int att)
   static float plane1[3]={1,10,100};
   static float plane2[3]={1,1.468,100};
   static float plane3[3]={1,2.154,4.642};
-  return plane1[(att/100)%1000]*plane2[(att/10)%100]*plane3[att%10];
+  return plane1[(att%1000)/100-1]*plane2[(att%100)/10-1]*plane3[att%10-1];
 }
 
 
