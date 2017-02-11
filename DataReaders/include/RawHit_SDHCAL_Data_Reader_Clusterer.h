65b6c3712fec83a10401c492d9eeab7eee1e4b24
Gerald <grenier@ipnl.in2p3.fr>
2017-02-12 00:33:29 +0100

    Time counting the clustering algorithm

--- DataReaders/include/RawHit_SDHCAL_Data_Reader_Clusterer.h
+++ DataReaders/include/RawHit_SDHCAL_Data_Reader_Clusterer.h
@@ -5,6 +5,8 @@
 #include "RawHit_SDHCAL_Data_Listener.h"
 #include "RawHit_SDHCAL_Data_Reader.h"
 
+#include "TimeCounter.h"
+
 template <class mergePredicate>
 class RawHit_SDHCAL_Data_Reader_Clusterer : public RawHit_SDHCAL_Data_Listener, public RawHit_SDHCAL_Data_Reader
 {
@@ -12,10 +14,10 @@ class RawHit_SDHCAL_Data_Reader_Clusterer : public RawHit_SDHCAL_Data_Listener, 
  RawHit_SDHCAL_Data_Reader_Clusterer(mergePredicate &f,bool verbose=false) : m_f(f), m_verbose(verbose), m_nevent(0) {}
   void process(const RawHit_SDHCAL_Data& d)
   {
-    RawHit_SDHCAL_Data newd(d.getHitVector(), d, d.getNumberOfEventInThisData()); //not terrific in terms of copy
-    newd.clusterize(m_f);
+    TimeCounters["CopyVec"].start(); RawHit_SDHCAL_Data newd(d.getHitVector(), d, d.getNumberOfEventInThisData()); TimeCounters["CopyVec"].stop();//not terrific in terms of copy
+    TimeCounters["Clus"].start(); newd.clusterize(m_f); TimeCounters["Clus"].stop(); 
     if (m_verbose) std::cout << ++m_nevent << " : " << newd.getHitVector().size() << " hits clustered in " << newd.getClusters().size() << " clusters.\r"; std::cout.flush();
-    notifyListeners(newd);
+    TimeCounters["Listen"].start(); notifyListeners(newd); TimeCounters["Listen"].stop();
   }
  private:
   mergePredicate &m_f;
--- RawCalorimeterHitInterface/include/TimeCounter.h
+++ RawCalorimeterHitInterface/include/TimeCounter.h
@@ -11,8 +11,8 @@ class TimeCounter
 {
  public:
  TimeCounter() : m_start(0), m_duration(0), m_NumberOfCall(0) {}
-  void start() {m_start=std::clock();}
-  void stop() {std::clock_t end=std::clock(); m_duration+=end-m_start; ++m_NumberOfCall;}
+  inline void start() {m_start=std::clock();}
+  inline void stop() {std::clock_t end=std::clock(); m_duration+=end-m_start; ++m_NumberOfCall;}
   double elapsedTime() const {return m_duration/CLOCKS_PER_SEC;}
   double elapsedTimePerCall() const {return m_NumberOfCall==0 ? m_NumberOfCall : elapsedTime()/m_NumberOfCall;}
  private:
