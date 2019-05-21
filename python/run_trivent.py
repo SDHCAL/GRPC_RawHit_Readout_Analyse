269b6ebee0f2df8c467c97aa81003aaff962e64d
GRENIER Gerald <g.grenier@ipnl.in2p3.fr>
2019-05-21 12:30:40 +0200

    Add script to run trivent (set for SPS 2018 data)

---
+++ python/run_trivent.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+
+#to be executed in the lib directory where all the needed libraries are installed 
+# usage python <name>.py  list_of_slcio_files
+
+import sys
+import os
+import ROOT
+ROOT.gROOT.Reset()
+ROOT.gSystem.Load('liblcio')
+ROOT.gSystem.Load('libGRPC_RawHit_Readout_Analyser')
+ROOT.gSystem.Load('libGRPC_RawHit_Readout_Analyser_dict')
+if ROOT.gROOT.GetVersion()[0]=='6':
+  #ROOT 6.08.02 don't understand non template dictionnary without it (don't know why)
+  dummy=ROOT.intervalle('unsigned int')()
+
+inputFileNames=ROOT.vector("string")()
+for f in sys.argv[1:]:
+    inputFileNames.push_back(f);
+
+for file in inputFileNames:
+    print file
+
+
+    
+outputFileName=os.path.splitext(os.path.basename(inputFileNames[0]))[0]+'_TriventSplit.slcio'
+print "output file name is ", outputFileName
+
+
+    
+experience=ROOT.CERN_SPS_Sept2018_SDHCAL_ExperimentalSetup()
+numeroBIF=experience.getBIF()
+
+print numeroBIF
+BIFtriggerWindow=ROOT.intervalle('int')(-7,-6)
+
+    
+#start LCIO reader
+lcReader=ROOT.IOIMPL.LCFactory.getInstance().createLCReader()
+
+#create architecture of listeners
+masterReader=ROOT.RawHit_SDHCAL_Data_Reader_From_LCEvent()
+lcReader.registerLCEventListener(masterReader)
+
+BIFListener=ROOT.BIF_Data_Listener(numeroBIF)
+masterReader.registerDataListener(BIFListener)
+
+trivent=ROOT.RawHit_SDHCAL_Data_Reader_Trivent(2,7) # 2=event windows half size, 7=threshold number for hits
+trivent.setSkipIfBIFisOutsideReadout(False)
+trivent.setBIFparameters(numeroBIF,BIFtriggerWindow) # event window = [-2,2] so BIF window = [-9,-4]
+#trivent.setUltraVerboseDebugOutput(True)
+masterReader.registerDataListener(trivent)
+
+hitOccupancy=ROOT.RawHit_Occupancy_Listener()
+trivent.registerDataListener(hitOccupancy)
+
+BIFListener_check=ROOT.BIF_Data_Listener(numeroBIF)
+trivent.registerDataListener(BIFListener_check)
+
+LCIOoutputWriter=ROOT.RawHit_SDHCAL_Data_LCWriter_RawCalorimeterHit()
+LCIOoutputWriter.open(outputFileName)
+trivent.registerDataListener(LCIOoutputWriter)
+
+#open file and event loop
+lcReader.open( inputFileNames )
+#lcReader.skipNEvents(63)
+#lcReader.readStream(2)
+lcReader.readStream()
+
+#end of event loop
+BIFListener_check.printMaxDelay()
+
+rootFile=ROOT.TFile("noisetrivent_check.root"  , "RECREATE")
+#load ROOT library missing
+ROOT.TH1F
+d=rootFile.mkdir("RawHits")
+hitOccupancy.saveTo(d,experience)
+
+dbis=rootFile.mkdir("BIFDelay")
+BIFListener.saveTo(dbis)
+
+dd=rootFile.mkdir("BIFDelay_check")
+BIFListener_check.saveTo(dd)
+
+rootFile.Close()
+
+lcReader.close()
+LCIOoutputWriter.close()
