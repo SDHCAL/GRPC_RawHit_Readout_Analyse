55a6777b52a2b29a542947d9ff413e433a861d1a
GRENIER Gerald <g.grenier@ipnl.in2p3.fr>
2019-05-22 11:36:13 +0200

    Expand test for experimental setup

--- ExperimentalSetup/src/test_ExperimentalSetup.cpp
+++ ExperimentalSetup/src/test_ExperimentalSetup.cpp
@@ -18,7 +18,23 @@ void testExperimentalSetup()
   aSetup.addSDHCALPlan(300,301,302);
   aSetup.addCMSstrip(215,216);
   assert(aSetup.hasBIF());
-  //assert(aSetup.
+  assert(aSetup.getBIF()==2);
+  assert(aSetup.DIFnumberIsBIF(2));
+  assert(aSetup.DIFnumberIsKnown(2));
+  assert(aSetup.DIFnumberIsKnown(14));
+  assert(aSetup.DIFnumberIsKnown(300));
+  assert(aSetup.DIFnumberIsKnown(215));
+  assert(aSetup.DIFnumberIsStrip(215));
+  assert(aSetup.DIFnumberIsStrip(216));
+  assert(aSetup.DIFnumberIsPad(14));
+  assert(aSetup.DIFnumberIsPad(15));
+  assert(aSetup.DIFnumberIsPad(300));
+  assert(aSetup.DIFnumberIsPad(301));
+  assert(aSetup.DIFnumberIsPad(302));
+  assert(aSetup.PlanIsPad(0));
+  assert(aSetup.PlanIsPad(1));
+  assert(aSetup.PlanIsPad(2));
+  assert(aSetup.PlanIsStrip(3));
 }
 
 
