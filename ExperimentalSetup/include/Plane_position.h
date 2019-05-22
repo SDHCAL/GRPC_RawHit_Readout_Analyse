6523b2968a8be9586328008af739cfc8c4e6e1e1
GRENIER Gerald <g.grenier@ipnl.in2p3.fr>
2019-05-22 15:25:26 +0200

    Add setters to Plane_position

--- ExperimentalSetup/include/Plane_position.h
+++ ExperimentalSetup/include/Plane_position.h
@@ -7,7 +7,10 @@
 class Plane_position
 {
  public:
-  Plane_position() {;}
+  Plane_position() : position_origin({0,0,0}), direction_I({1,0,0}), direction_J({0,1,0}) {;}
+  void setPosition(std::array<double,3> d) {position_origin=d;}
+  void setDirections(std::array<double,3> I,std::array<double,3> J) {direction_I=I;direction_J=J;normalize_directions();}
+  //void getPosition(double xI, double yI, double* pos)
  private:
   std::array<double,3> position_origin;
   std::array<double,3> direction_I;
