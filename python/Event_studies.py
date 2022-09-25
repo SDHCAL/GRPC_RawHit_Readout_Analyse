61287156b2f14425e480b934264ca3a3aba3dbe2
GRENIER Gerald <g.grenier@ipnl.in2p3.fr>
2022-09-25 04:41:31 +0200

    Add event study to look at noisy ASICs

--- python/Event_studies.py
+++ python/Event_studies.py
@@ -5,11 +5,23 @@ import pyLCIO
 def doNothing(col):
     pass
 
-def test_a(col):
-    print ("test A")
-
-def test_b(col):
-    print ("test_B")
+def asic_study(col):
+    q=pyLCIO.UTIL.CellIDDecoder('EVENT::CalorimeterHit')(col)
+    asicStat={}
+    for hit in col:
+        key=(q(hit)["DIF"].value(),q(hit)["ASIC"].value())
+        if key in asicStat:
+            asicStat[key]+=1
+        else:
+            asicStat[key]=1
+    highest_ASIC=sorted(asicStat.values())[-1:][0]
+    two_highest_ASIC=sum(sorted(asicStat.values())[-2:])
+    three_highest_ASIC=sum(sorted(asicStat.values())[-3:])
+    sum_ASIC=sum(asicStat.values())
+    print (highest_ASIC,sum_ASIC,highest_ASIC/sum_ASIC)
+    print (two_highest_ASIC,sum_ASIC,two_highest_ASIC/sum_ASIC)
+    print (three_highest_ASIC,sum_ASIC,three_highest_ASIC/sum_ASIC)
+    
 
 
     
